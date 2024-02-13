23945ea15fceb0704f26ba8290c0f6d49aca4e0d
Luca112869 <115638439+Luca112869@users.noreply.github.com>
2024-02-13 12:46:55 +0000

    jtg

--- db/create.sql
+++ db/create.sql
@@ -11,7 +11,7 @@ CREATE table product (
   stof_ID integer,
   merk_ID integer,
   maat_ID integer,
-  categorie_ID integer
+  categories_ID integer
 );
 
 CREATE TABLE kleur ( 
@@ -74,37 +74,33 @@ CREATE table stof_kleding (
 
 
 
-insert into products (name, description, code, price, categories_id, merk_id) values ('trui', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5,1 ,2);
+insert into product (name, description, code, price, categories_id, merk_id) values ('trui', 'warme trui voor de koude winterdagen.', '816905633-0', 10.5,1 ,2);
 --1 is dan de categorie = katoen en 2 is merk = adidas
-insert into products (name, description, code, price) values ('lelekijkschoen', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('tomaat, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('komkommer, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('kip, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('vis', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into product (name, description, code, price) values ('schoenen', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
+insert into product (name, description, code, price) values ('tomaat, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
+insert into product (name, description, code, price) values ('komkommer, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
+insert into product (name, description, code, price) values ('kip, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
+insert into product (name, description, code, price) values ('vis', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
 
-insert into merk (naam) values (nike);
-insert into merk (naam) values (adidas);
-insert into stof_kleding () values ();
-insert into stof_kleding () values ();
-insert into stof_kleding () values ();
-insert into stof_kleding () values ();
+insert into merk (naam) values ('nike');
+insert into merk (naam) values ('adidas');
+insert into merk (naam) values ('stone island');
+insert into merk (naam) values ('zara');
+insert into merk (naam) values ('lyle en scott');
 
-insert into categories (soortkleding) values (katoen);
-insert into categories (soortkleding) values (wol);
-insert into categories (soortkleding) values ();
-insert into categories (soortkleding) values ();
-insert into categories (soortkleding) values ();
-insert into categories (soortkleding) values ();
-
-insert into  () values ();
-insert into  () values ();
-insert into  () values ();
-insert into  () values ();
-insert into  () values ();
-insert into  () values ();
+insert into categories (soortkleding) values ('katoen');
+insert into categories (soortkleding) values ('wol');
+insert into categories (soortkleding) values ('polyester');
+insert into categories (soortkleding) values ('linnen');
+insert into categories (soortkleding) values ('zijde');
 
 
-
+--insert into  () values ();
+--insert into  () values ();
+--insert into  () values ();
+--insert into  () values ();
+--insert into  () values ();
+--insert into  () values ();
 
 
 
@@ -112,11 +108,10 @@ insert into  () values ();
 
 
 insert into stof_kleding (wasvoorschrift, stof) values ('warm wassen in wasmachine', 'katoen');
-insert into stof_kleding (name, description, code, price) values ('lelekijkschoen', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into stof_kleding (name, description, code, price) values ('tomaat, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into stof_kleding (name, description, code, price) values ('komkommer, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into stof_kleding (name, description, code, price) values ('kip, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into stof_kleding (name, description, code, price) values ('vis', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into stof_kleding (wasvoorschrift, stof) values ('met de hand wassen in warm water', 'wol');
+insert into stof_kleding (wasvoorschrift, stof) values ('koud wassen in wasmachine', 'polyester');
+insert into stof_kleding (wasvoorschrift, stof) values ('met de hand wassen in warm water', 'linnen');
+insert into stof_kleding (wasvoorschrift, stof) values ('koud wassen in wasmachine', 'zijde');
 
 insert into kleur (kleur_naam) values ('blauw');
 insert into kleur (kleur_naam) values ('lichtblauw');
@@ -132,8 +127,7 @@ insert into kleur (kleur_naam) values ('lichtblauw');
   insert into kleur (kleur_naam) values ('blauw');--nog kijken
 
   
-
-insert into product_kleur (product_id, , kleur_id) values (1, 2 );--eerste cijfer is product tweede is kleur
+insert into product_kleur (product_id, , kleur_id) values (1, 1 );--eerste cijfer is product tweede is kleur
 insert into product_kleur (product id, , kleur id) values (1, 3 );
 insert into product_kleur (product id, , kleur id) values (2, 1 );
 insert into product_kleur (product id, , kleur id) values (1, 2 );
